26a75ae7d0cf28ea6d49b7af500523a5175fbaaa
weavedawg74 <weavedawg74@yahoo.com>
2020-04-26 10:50:46 -0500

    first commit

---
+++ DiscussionBoard/boards/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Board
+
+# Create your views here.
+def home(request):
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})   
