be66e0b2dd1a1b916d3f4413066815f807361a58
weavedawg74 <weavedawg74@yahoo.com>
2020-05-30 14:46:57 -0500

    prepped deploiyment

--- DiscussionBoard/DiscussionBoard/settings.py
+++ DiscussionBoard/DiscussionBoard/settings.py
@@ -129,6 +129,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_REDIRECT_URL = 'home'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
